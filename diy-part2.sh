436c287710138f51e1cb6f4bab9cf0030fe54301
whjddzyx <58959156+whjddzyx@users.noreply.github.com>
2020-11-30 00:30:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,71 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+device_name='E8820S'
+wifi_name='wifi'
+lan_ip='10.0.11.254'        # Lan Ip地址
+utc_name='Asia\/Shanghai'   # 时区
+#openClash_url='https://github.com/vernesong/OpenClash.git'       # OpenClash包地址 
+
+
+ 
+#修改机器名称
+echo "设置主机名"
+sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo "设置lan ip"
+sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+
+#修改默认时区
+echo "修改时区"
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+
+echo "修改wifi名称"
+
+sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+
+#echo "替换文件"
+#rm -rf ./target/linux/ramips/dts/HC5962.dts && cd .. && cp -f ./HC5962.dts openwrt/target/linux/ramips/dts/ && cd openwrt
+#rm -rf ./target/linux/ramips/base-files/etc/board.d/02_network && cd .. && cp -f ./02_network openwrt/target/linux/ramips/base-files/etc/board.d/ && cd openwrt
+
+#echo '添加OpenClash'
+#git clone -master $openClash_url package/lean/luci-app-openclash 
+#echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
+#echo 'CONFIG_PACKAGE_luci-i18n-openclash-zh-cn=y'  >> .config
+
+#echo '添加Passwall'
+
+#echo 'CONFIG_PACKAGE_luci-app-wol=n' >> .config
+echo 'CONFIG_PACKAGE_luci-app-unblockmusic=n' >> .config
+echo 'CONFIG_PACKAGE_luci-app-ssr-plus=n' >> .config
+echo 'CONFIG_PACKAGE_CONFIG_PACKAGE_samba=n' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-app-passwall=y' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan=y' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=y' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=y' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y' >> .config
+
+#echo 'CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y'  >> .config
+
+
